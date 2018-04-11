ccd7ed24f7c5d16908d9c504b0918a50fcd6c1c3
Hiday Eichler <hiday1979gamil.com@Hidays-MacBook-Pro.local>
2018-04-11 10:31:40 +0300

    make it better

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AbuteComponent } from './abute/abute.component';
-
+import { FotterComponent } from './fotter/fotter.component';
+import { HeaderComponent } from './header/header.component';
+import { MainPicComponent } from './main-pic/main-pic.component';
+import { DragulaModule } from 'ng2-dragula';
+import { MyServiceService} from './my-service.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AbuteComponent
+    AbuteComponent,
+    FotterComponent,
+    HeaderComponent,
+    MainPicComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    DragulaModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [MyServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,11 @@
+import { AbuteComponent } from './../abute/abute.component';
 import { Component, OnInit } from '@angular/core';
+import { DragulaService } from 'ng2-dragula/ng2-dragula';
+import { FotterComponent } from '../fotter/fotter.component';
+import { HeaderComponent } from '../header/header.component';
+import { MyServiceService} from '../my-service.service';
+
+
 
 @Component({
   selector: 'app-home',
@@ -7,12 +14,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  itemCount = 4;
-  btnText = 'Add an item';
+  imgCount = 42;
+  imgSrc = '/assets/img/air_temp_max.png';
+  goalText = 'My first life goal';
+  images = ['/assets/img/air_temp_max.png', '/assets/img/air_temp_max.png', '/assets/img/air_temp_max.png', '/assets/img/air_temp_max.png'];
+  options: any = {
+    removeOnSpill: true
+};
 
-  constructor() { }
+
+  constructor(private _data: MyServiceService) {
+  }
 
   ngOnInit() {
+  }
+
+   doubleClickImg() {
+    // const upLoad = document.createElement('INPUT');
+    // upLoad.setAttribute('type', 'file');
+    // document.getElementById('mainPic').appendChild(upLoad);
+    // document.getElementById('demo').setAttribute('src', '/assets/img/imgPlaceHolder.png');
   }
 
 }
